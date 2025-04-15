5411910987386db763b5611ed3e13cfb192b1148
jacquelinesantana <ti.jacque@gmail.com>
2025-04-15 11:49:01 -0300

    Atualizações de favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navbar from './componentes/navbar/Navbar'
 import Footer from './componentes/footer/Footer'
