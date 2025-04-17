194c948ca2a064e39e8062f92388d4f72ef1495d
jacquelinesantana <ti.jacque@gmail.com>
2025-04-17 14:09:10 -0300

    banner

--- src/componentes/banner/Banner.tsx
+++ src/componentes/banner/Banner.tsx
@@ -9,12 +9,12 @@ function Banner() {
       <div
         className="absolute top-0 left-0 w-full h-full bg-cover bg-center md:bg-fixed md:bg-contain"
         style={{
-          backgroundImage: "url('https://i.imgur.com/Kf6cKmh.png')",
+          backgroundImage: "url('https://i.imgur.com/dPfG7i4.gif')",
           backgroundAttachment: "fixed ", // o segredo do efeito parallax aqui
         }}
       />
       <div className="relative z-10 flex items-center justify-center h-full ">
-        <h1 className="text-4xl md:text-6xl text-rose-800 font-bold text-center">
+        <h1 className="text-4xl md:text-6xl text-lime-300 font-bold text-center">
         Criação de Sites e Sistemas Personalizados para Impulsionar Seu Negócio
         </h1>
       </div>
