000f172fd93571b6f47bb5a57f0f895b2ef3ee87
jacquelinesantana <ti.jacque@gmail.com>
2025-04-17 21:37:14 -0300

    home voltou

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-
 import Footer from "./components/footer"
 import Header from "./components/header"
 import Home from "./pages/Home/Home"
+
+
 
 
 function App() {
   return (
     <>
       <Header />
+      
       <Home />
 
-      <Footer />
-      
+     <Footer />
+
     </>
   )
 }
