8cc89160dc4728354e87720c849b741faa956098
jacquelinesantana <ti.jacque@gmail.com>
2025-04-19 00:49:46 -0300

    ajustes card

--- src/components/ui/Cards.tsx
+++ src/components/ui/Cards.tsx
@@ -7,12 +7,12 @@ interface cardProps{
 function Cards(props: cardProps) {
   return (
     <>
-      <div className="w-5/6 sm:w-1/2 p-6">
-        <div className="card bg-base-100 image-full w-96 shadow-sm">
+      <div className="w-5/6 sm:w-5/2 p-6 sm:p-2 md:w-5/2 ">
+        <div className="card bg-base-100 image-full  shadow-sm">
             <figure>
                 <img
                 src={props.foto}
-                alt="Shoes" className="w-full"/>
+                alt="Shoes" className=""/>
             </figure>
             <div className="card-body mt-18">
                 <h2 className="card-title">{props.nome}</h2>
